7edfdae988cf24119a6e81cbeb07ea5d8a9ecb01
DogMey <68884758+DogMey@users.noreply.github.com>
2021-12-16 17:54:38 -0500

    Plantilla instancias :3

--- Patrones_Creacionales_I/src/Principal/CuerpoaCuerpoFactory.java
+++ Patrones_Creacionales_I/src/Principal/CuerpoaCuerpoFactory.java
@@ -3,10 +3,10 @@ package Principal;
 public class CuerpoaCuerpoFactory implements MiliciaFactory{
 
     public void crearGuerrero() {
-
+        CuerpoCuerpoGuerrero guerrero = new CuerpoCuerpoGuerrero();
     }
 
     public void crearMaquina() {
-
+        CuerpoCuerpoMaquina maquina = new CuerpoCuerpoMaquina();
     }
 }
--- Patrones_Creacionales_I/src/Principal/LargaDistanciaFactory.java
+++ Patrones_Creacionales_I/src/Principal/LargaDistanciaFactory.java
@@ -3,10 +3,16 @@ package Principal;
 public class LargaDistanciaFactory implements MiliciaFactory{
 
     public void crearGuerrero() {
-
+        LargaDistanciaGuerrero guerrero = new LargaDistanciaGuerrero();
+        String tipo = guerrero.tipoPersonaje();
+        System.out.println(tipo);
+        System.out.println("\t"+ guerrero.defVida(tipo));
+        System.out.println("\t"+ guerrero.defDaño());
+        System.out.println("\t"+ guerrero.defVelocidad());
+        System.out.println("\t"+ guerrero.defAlcance(tipo));
     }
 
     public void crearMaquina() {
-
+        LargaDistanciaMaquina maquina = new LargaDistanciaMaquina();
     }
 }
