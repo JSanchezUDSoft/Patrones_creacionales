3f517fe6391b2992c6f04c4b301c578313bc69be
Diego-1116 <72049443+Diego-1116@users.noreply.github.com>
2021-12-16 18:10:06 -0500

    cambios en factoria de cuerpo a cuerpo

--- Patrones_Creacionales_I/src/Principal/CuerpoaCuerpoFactory.java
+++ Patrones_Creacionales_I/src/Principal/CuerpoaCuerpoFactory.java
@@ -4,9 +4,23 @@ public class CuerpoaCuerpoFactory implements MiliciaFactory{
 
     public void crearGuerrero() {
         CuerpoCuerpoGuerrero guerrero = new CuerpoCuerpoGuerrero();
+        String tipo = guerrero.tipoPersonaje();
+        System.out.println(tipo);
+        System.out.println("\t"+ guerrero.defVida(tipo));
+        System.out.println("\t"+ guerrero.defDaño());
+        System.out.println("\t"+ guerrero.defVelocidad());
+        System.out.println("\t"+ guerrero.defAlcance(tipo));
+
     }
 
     public void crearMaquina() {
         CuerpoCuerpoMaquina maquina = new CuerpoCuerpoMaquina();
+        String tipo = maquina.tipoMaquina();
+        System.out.println(tipo);
+        System.out.println("\t"+ maquina.defVida(tipo));
+        System.out.println("\t"+ maquina.defDaño(tipo));
+        System.out.println("\t"+ maquina.defVelocidad());
+        System.out.println("\t"+ maquina.defAlcance(tipo));
+
     }
 }
