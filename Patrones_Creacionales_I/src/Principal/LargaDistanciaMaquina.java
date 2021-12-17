4f817a0c6ad920973fede5114c72f3257b7a46d4
Javier Sanchez <jsanchez0508@hotmail.com>
2021-12-17 09:46:18 -0500

    Random LargaDistanciaMaquina.java

--- Patrones_Creacionales_I/src/Principal/LargaDistanciaMaquina.java
+++ Patrones_Creacionales_I/src/Principal/LargaDistanciaMaquina.java
@@ -4,7 +4,7 @@ public class LargaDistanciaMaquina implements Maquina{
 
     public String tipoMaquina() {
         try {
-            switch ((int) (Math.random() * 2 + 1)) {
+            switch ((int) (Math.random() * (2 - 1 + 1)+1)) {
                 case 1:
                     return "Catapulta";
                 case 2:
@@ -22,9 +22,9 @@ public class LargaDistanciaMaquina implements Maquina{
     public int defVida(String tipoMaquina){
         try {
             if (tipoMaquina.equals("Catapulta")) {
-                return (int) (Math.random() * 250 + 200);
+                return (int) (Math.random() * (250 - 200 + 1)+200);
             } else {
-                return (int) (Math.random() * 230 + 190);
+                return (int) (Math.random() * (230 - 190 + 1)+190);
             }
         }
         catch(Exception e){
@@ -36,9 +36,9 @@ public class LargaDistanciaMaquina implements Maquina{
     public int defDaño(String tipoMaquina){
         try {
             if (tipoMaquina.equals("Catapulta")) {
-                return (int) (Math.random() * 90 + 80);
+                return (int) (Math.random() * (90 - 80 + 1)+80);
             } else {
-                return (int) (Math.random() * 120 + 100);
+                return (int) (Math.random() * (120 - 100)+100);
             }
         }
         catch(Exception e){
@@ -49,7 +49,7 @@ public class LargaDistanciaMaquina implements Maquina{
 
     public int defVelocidad(){
         try {
-            return (int) (Math.random() * 7 + 4);
+            return (int) (Math.random() * (7 - 4 + 1)+4);
         }
         catch(Exception e){
             System.out.println("Se presento un error en LargaDistanciaMaquina.defVelocidad " + e);
@@ -60,9 +60,9 @@ public class LargaDistanciaMaquina implements Maquina{
     public int defAlcance(String tipoMaquina) {
         try {
             if (tipoMaquina.equals("Catapulta")) {
-                return (int) (Math.random() * 35 + 30);
+                return (int) (Math.random() * (35 - 30 + 1)+30);
             } else {
-                return (int) (Math.random() * 30 + 20);
+                return (int) (Math.random() * (30 - 20 + 1)+20);
             }
         }
         catch(Exception e){
